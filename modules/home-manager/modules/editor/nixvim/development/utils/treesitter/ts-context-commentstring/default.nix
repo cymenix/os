598645a3adcef0208bf80ba912d1262f857db142
Clemens Horn <clemens.horn@mni.thm.de>
2024-06-18 23:04:06 +0200

    fix(nixvim): use fork for nvim-ts-context-commentstring
    broken for haskell otherwise

--- modules/home-manager/modules/editor/nixvim/development/utils/treesitter/ts-context-commentstring/default.nix
+++ modules/home-manager/modules/editor/nixvim/development/utils/treesitter/ts-context-commentstring/default.nix
@@ -1,10 +1,25 @@
-{...}: {
+{pkgs, ...}: let
+  ts-context-commentstring = pkgs.vimUtils.buildVimPlugin {
+    name = "ts-context-commentstring";
+    src = pkgs.fetchFromGitHub {
+      owner = "mrcjkb";
+      repo = "nvim-ts-context-commentstring";
+      rev = "c6eebaa0288ec7c0b3df9fcef83fc4ae8bb39c49";
+      hash = "sha256-O+5wJtWFs4ZxD9q0+SxR9ggd5amUdnoTbRItaN7rB5E=";
+    };
+  };
+in {
   programs = {
     nixvim = {
+      extraPlugins = [ts-context-commentstring];
+      extraConfigLuaPost =
+        /*
+        lua
+        */
+        ''
+          require('ts_context_commentstring').setup {}
+        '';
       plugins = {
-        ts-context-commentstring = {
-          enable = true;
-        };
         comment = {
           enable = true;
           settings = {

358dcca20ad4e0ca9e3d24c932a95da22e9f3ef2
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-02 18:17:31 +0200

    chore(nix): add dolphin explorer

--- modules/home-manager/modules/display/qt/default.nix
+++ modules/home-manager/modules/display/qt/default.nix
@@ -34,7 +34,6 @@ in
         sessionVariables = {
           QT_QPA_PLATFORM = "wayland";
           QT_QPA_PLATFORMTHEME = "qt5ct";
-          QT_STYLE_OVERRIDE = mkForce "";
           QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
           QT_AUTO_SCREEN_SCALE_FACTOR = "1";
         };

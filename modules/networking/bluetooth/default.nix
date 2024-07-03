2f679e2946b518e9bf71a66b7811ea3577ccd3e3
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-03 12:15:01 +0200

    chore(nix): update

--- modules/networking/bluetooth/default.nix
+++ modules/networking/bluetooth/default.nix
@@ -51,6 +51,26 @@ in
         blueman = {
           enable = cfg.bluetooth.enable;
         };
+        udev = {
+          packages = [
+            (pkgs.writeTextFile {
+              text = ''
+                KERNEL=="uinput", MODE="0666"
+                KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
+                KERNEL=="hidraw*", SUBSYSTEM=="hidraw", KERNELS=="0005:054C:05C4.*", MODE="0666"
+                KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="09cc", MODE="0666"
+                KERNEL=="hidraw*", SUBSYSTEM=="hidraw", KERNELS=="0005:054C:09CC.*", MODE="0666"
+                # Valve HID devices over bluetooth hidraw
+                KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666"
+                # Dualshock 4 over bluetooth hidraw
+                KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0666"
+                # Dualshock 4 Slim over bluetooth hidraw
+                KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0666"
+              '';
+              destination = "/etc/udev/rules.d/99-ds4-controller.rules";
+            })
+          ];
+        };
       };
     };
   }

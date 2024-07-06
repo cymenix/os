1be2168437a35cea381b2379b5e09d4c97e9bc1c
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-06 16:19:51 +0200

    chore(reversing): overlay imhex

--- modules/home-manager/modules/development/reversing/imhex/default.nix
+++ modules/home-manager/modules/development/reversing/imhex/default.nix
@@ -1,10 +1,41 @@
 {
-  pkgs,
+  nixpkgs,
+  system,
   config,
   lib,
   ...
 }: let
   cfg = config.modules.development.reversing;
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = [
+      (self: super: {
+        imhex = super.imhex.overrideAttrs (finalAttrs: previousAttrs:
+          with previousAttrs; let
+            patterns_version = "1.35.3";
+            patterns_src = super.fetchFromGitHub {
+              owner = "WerWolv";
+              repo = "ImHex-Patterns";
+              rev = "ImHex-v${patterns_version}";
+              hash = "sha256-h86qoFMSP9ehsXJXOccUK9Mfqe+DVObfSRT4TCtK0rY=";
+            };
+          in {
+            version = "1.35.3";
+            src = super.fetchFromGitHub {
+              fetchSubmodules = true;
+              owner = "WerWolv";
+              repo = pname;
+              rev = "v${version}";
+              hash = "sha256-8vhOOHfg4D9B9yYgnGZBpcjAjuL4M4oHHax9ad5PJtA=";
+            };
+            postInstall = ''
+              mkdir -p $out/share/imhex
+              rsync -av --exclude="*_schema.json" ${patterns_src}/{constants,encodings,includes,magic,patterns} $out/share/imhex
+            '';
+          });
+      })
+    ];
+  };
 in
   with lib; {
     options = {

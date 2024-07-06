12d98fcebb365fed796713d0c28322bf054d24b1
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-06 16:31:11 +0200

    fix(imhex): hash

--- modules/home-manager/modules/development/reversing/imhex/default.nix
+++ modules/home-manager/modules/development/reversing/imhex/default.nix
@@ -10,29 +10,28 @@
     inherit system;
     overlays = [
       (self: super: {
-        imhex = super.imhex.overrideAttrs (finalAttrs: previousAttrs:
-          with previousAttrs; let
-            patterns_version = "1.35.3";
-            patterns_src = super.fetchFromGitHub {
-              owner = "WerWolv";
-              repo = "ImHex-Patterns";
-              rev = "ImHex-v${patterns_version}";
-              hash = "sha256-h86qoFMSP9ehsXJXOccUK9Mfqe+DVObfSRT4TCtK0rY=";
-            };
-          in {
-            version = "1.35.3";
-            src = super.fetchFromGitHub {
-              fetchSubmodules = true;
-              owner = "WerWolv";
-              repo = pname;
-              rev = "v${version}";
-              hash = "sha256-8Ehpk0TjE4itQ7D9Nx74plYwABVufuYmxfxyuSqak1c=";
-            };
-            postInstall = ''
-              mkdir -p $out/share/imhex
-              rsync -av --exclude="*_schema.json" ${patterns_src}/{constants,encodings,includes,magic,patterns} $out/share/imhex
-            '';
-          });
+        imhex = super.imhex.overrideAttrs (finalAttrs: previousAttrs: let
+          patterns_version = "1.35.3";
+          patterns_src = super.fetchFromGitHub {
+            owner = "WerWolv";
+            repo = "ImHex-Patterns";
+            rev = "ImHex-v${patterns_version}";
+            hash = "sha256-h86qoFMSP9ehsXJXOccUK9Mfqe+DVObfSRT4TCtK0rY=";
+          };
+        in rec {
+          version = "1.35.3";
+          src = super.fetchFromGitHub {
+            fetchSubmodules = true;
+            owner = "WerWolv";
+            repo = previousAttrs.pname;
+            rev = "v${version}";
+            hash = "sha256-8vhOOHfg4D9B9yYgnGZBpcjAjuL4M4oHHax9ad5PJtA=";
+          };
+          postInstall = ''
+            mkdir -p $out/share/imhex
+            rsync -av --exclude="*_schema.json" ${patterns_src}/{constants,encodings,includes,magic,patterns} $out/share/imhex
+          '';
+        });
       })
     ];
   };

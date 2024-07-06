eb4eefd1ba30a949addfbd3454f8e3f24a6f1dc9
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-06 13:14:57 +0200

    chore(reversing): ps3ida derivation

--- modules/home-manager/modules/development/reversing/ida/default.nix
+++ modules/home-manager/modules/development/reversing/ida/default.nix
@@ -1,10 +1,35 @@
 {
-  pkgs,
+  nixpkgs,
+  system,
   config,
   lib,
   ...
 }: let
   cfg = config.modules.development.reversing;
+  ps3IdaOverlay = _: super: {
+    ps3ida = super.stdenv.mkDerivation rec {
+      name = "ps3ida";
+      version = "3d38ddf6a6bc879531ae0434770eef8eed07d7c8";
+      src = super.fetchFromGithub {
+        owner = "kakaroto";
+        repo = name;
+        rev = version;
+        hash = "sha256-JiE0Q0bQzsEmT6xME6Rv1LoBwWLYHJ2KMl44QdkaZZw=";
+      };
+      phases = "install";
+      installPhase = ''
+        mkdir -p $out
+        cp -r $src $out
+      '';
+    };
+  };
+  pkgs = import nixpkgs {
+    inherit system;
+    config = {
+      allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) ["ida-free"];
+    };
+    overlays = [ps3IdaOverlay];
+  };
 in
   with lib; {
     options = {
@@ -20,7 +45,7 @@ in
     };
     config = mkIf (cfg.enable && cfg.ida.enable) {
       home = {
-        packages = with pkgs; [ida-free];
+        packages = with pkgs; [ida-free ps3ida];
       };
     };
   }

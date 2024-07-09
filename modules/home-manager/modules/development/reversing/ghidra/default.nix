78737c0b4283dae851e8fd96dfbfa7db49c95958
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-09 18:17:04 +0200

    chore(ghidra): update

--- modules/home-manager/modules/development/reversing/ghidra/default.nix
+++ modules/home-manager/modules/development/reversing/ghidra/default.nix
@@ -9,31 +9,29 @@
   pkgs = import nixpkgs {
     inherit system;
     overlays = [
-      (self: super: {
-        ghidra = super.ghidra.overrideAttrs (oldAttrs: {
-          patches = oldAttrs.patches ++ [./powerpc.patch];
-        });
-        ghidra-extensions = with super.ghidra-extensions; {
-          ps3GhidraScripts = pkgs.stdenv.mkDerivation {
+      (self: super:
+        with super; {
+          ghidra = ghidra.overrideAttrs (oldAttrs: {
+            patches = oldAttrs.patches ++ [./powerpc.patch];
+          });
+          ps3GhidraScripts = stdenv.mkDerivation {
             name = "Ps3GhidraScripts";
-            src = pkgs.fetchurl {
+            src = fetchurl {
               url = "https://github.com/clienthax/Ps3GhidraScripts/releases/download/1.069/ghidra_11.0_PUBLIC_20240204_Ps3GhidraScripts.zip";
               sha256 = "04iqfgz1r1a08r2bdd9nws743a7h9gdxqfdf3dxbx10xqnpnwny8";
             };
-            nativeBuildInputs = with pkgs; [unzip];
+            nativeBuildInputs = [unzip];
             phases = "installPhase";
             installPhase = ''
               runHook preInstall
 
-              ls -la $src
               mkdir -p $out/lib/ghidra/Ghidra/Extensions
               unzip -d $out/lib/ghidra/Ghidra/Extensions $src
 
               runHook postInstall
             '';
           };
-        };
-      })
+        })
     ];
   };
 in
@@ -54,13 +52,13 @@ in
         packages =
           (with pkgs; [
             ghidra
+            ps3GhidraScripts
           ])
           ++ (with pkgs.ghidra-extensions; [
-            gnudisassembler
-            sleighdevtools
-            machinelearning
-            ghidraninja-ghidra-scripts
-            ps3GhidraScripts
+            # gnudisassembler
+            # sleighdevtools
+            # machinelearning
+            # ghidraninja-ghidra-scripts
           ]);
       };
     };

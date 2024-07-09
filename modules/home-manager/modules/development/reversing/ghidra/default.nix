03c869a192f719e84a928f3d4230bb02d9f3c4ee
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-09 17:59:55 +0200

    chore(ghidra): update

--- modules/home-manager/modules/development/reversing/ghidra/default.nix
+++ modules/home-manager/modules/development/reversing/ghidra/default.nix
@@ -13,26 +13,28 @@
         ghidra = super.ghidra.overrideAttrs (oldAttrs: {
           patches = oldAttrs.patches ++ [./powerpc.patch];
         });
+        ghidra-extensions = {
+          ps3GhidraScripts = pkgs.stdenv.mkDerivation {
+            name = "Ps3GhidraScripts";
+            src = pkgs.fetchurl {
+              url = "https://github.com/clienthax/Ps3GhidraScripts/releases/download/1.069/ghidra_11.0_PUBLIC_20240204_Ps3GhidraScripts.zip";
+              sha256 = "04iqfgz1r1a08r2bdd9nws743a7h9gdxqfdf3dxbx10xqnpnwny8";
+            };
+            nativeBuildInputs = with pkgs; [unzip];
+            phases = "installPhase";
+            installPhase = ''
+              runHook preInstall
+
+              ls -la $src
+              mkdir -p $out/lib/ghidra/Ghidra/Extensions
+              unzip -d $out/lib/ghidra/Ghidra/Extensions $src
+
+              runHook postInstall
+            '';
+          };
+        };
       })
     ];
-  };
-  ps3GhidraScripts = pkgs.stdenv.mkDerivation {
-    name = "Ps3GhidraScripts";
-    src = pkgs.fetchurl {
-      url = "https://github.com/clienthax/Ps3GhidraScripts/releases/download/1.069/ghidra_11.0_PUBLIC_20240204_Ps3GhidraScripts.zip";
-      sha256 = "04iqfgz1r1a08r2bdd9nws743a7h9gdxqfdf3dxbx10xqnpnwny8";
-    };
-    nativeBuildInputs = with pkgs; [unzip];
-    phases = "installPhase";
-    installPhase = ''
-      runHook preInstall
-
-      ls -la $src
-      mkdir -p $out/lib/ghidra/Ghidra/Extensions
-      unzip -d $out/lib/ghidra/Ghidra/Extensions $src
-
-      runHook postInstall
-    '';
   };
 in
   with lib; {

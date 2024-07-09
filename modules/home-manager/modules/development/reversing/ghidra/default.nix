fe6fa0a0335fd45a0d3fccac9fafad70a7cea45d
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-09 12:14:21 +0200

    chore(nix): update

--- modules/home-manager/modules/development/reversing/ghidra/default.nix
+++ modules/home-manager/modules/development/reversing/ghidra/default.nix
@@ -10,26 +10,8 @@
     inherit system;
     overlays = [
       (self: super: {
-        ghidra = super.ghidra.overrideAttrs (finalAttrs: previousAttrs: {
-          postPatch = ''
-            # Set name of release (eg. PUBLIC, DEV, etc.)
-            sed -i -e 's/application\.release\.name=.*/application.release.name=NIX/' Ghidra/application.properties
-
-            # Set build date and git revision
-            echo "application.build.date=$(cat SOURCE_DATE_EPOCH)" >> Ghidra/application.properties
-            echo "application.build.date.short=$(cat SOURCE_DATE_EPOCH_SHORT)" >> Ghidra/application.properties
-            echo "application.revision.ghidra=$(cat COMMIT)" >> Ghidra/application.properties
-
-            # Tells ghidra to use our own protoc binary instead of the prebuilt one.
-            cat >>Ghidra/Debug/Debugger-gadp/build.gradle <<HERE
-            protobuf {
-              protoc {
-                path = '${super.protobuf}/bin/protoc'
-              }
-            }
-            HERE
-            sed -i '/<\/unaffected>/i \        <register name="r2"/>' Ghidra/Processors/PowerPC/data/languages/ppc_64_32.cspec
-          '';
+        ghidra = super.ghidra.overrideAttrs (oldAttrs: {
+          patches = oldAttrs.patches ++ [./powerpc.patch];
         });
       })
     ];
@@ -65,6 +47,7 @@ in
             gnudisassembler
             sleighdevtools
             machinelearning
+            ghidraninja-ghidra-scripts
           ]);
       };
     };

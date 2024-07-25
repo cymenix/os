1c91fcceecee20f14eed4864a4ec56e5676f151c
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-25 23:40:35 +0200

    chore(browser): update firefox policies and preferences

--- modules/config/nix/default.nix
+++ modules/config/nix/default.nix
@@ -1,10 +1,10 @@
 {
   lib,
   config,
+  inputs,
   ...
 }: let
   cfg = config.modules.config;
-  user = config.modules.users.user;
 in
   with lib; {
     options = {
@@ -19,20 +19,45 @@ in
     config = mkIf (cfg.enable && cfg.nix.enable) {
       nix = {
         gc = {
-          automatic = true;
+          automatic = lib.mkDefault false;
           dates = "weekly";
           options = "--delete-older-than 30d";
         };
         optimise = {
           automatic = true;
         };
+        daemonCPUSchedPolicy = "idle";
+        daemonIOSchedClass = "idle";
+        registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
+        nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
         settings = {
           auto-optimise-store = true;
-          trusted-users = ["root" user];
+          builders-use-substitues = true;
+          keep-going = true;
+          allowed-users = ["@wheel"];
+          trusted-users = ["@wheel"];
           experimental-features = [
             "nix-command"
             "flakes"
             "repl-flake"
+          ];
+          substituters = [
+            "https://cache.nixos.org"
+            "https://nix-community.cachix.org"
+            "https://hyprland.cachix.org"
+            "https://yazi.cachix.org"
+            "https://nix-gaming.cachix.org"
+            "https://cache.iog.io"
+            "https://nixpkgs-unfree.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+            "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+            "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+            "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
           ];
         };
       };

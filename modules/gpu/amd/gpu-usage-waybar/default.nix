b05281b5eb754138442ba65c4fbcc865a9ad47c4
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-30 10:44:14 +0200

    chore(gpu-usage-waybar): update cargoHash

--- modules/gpu/amd/gpu-usage-waybar/default.nix
+++ modules/gpu/amd/gpu-usage-waybar/default.nix
@@ -9,5 +9,5 @@ with pkgs;
       rev = "b92311709f29a28c3f4f52172ae82d7ea34d1eac";
       hash = "sha256-NY6Xv2yg1QJO7enzJjtYXb/Zd1pNAtr0wENDQwerJ5w=";
     };
-    cargoHash = pkgs.lib.fakeHash;
+    cargoHash = "sha256-TOV+0cB2OmtsixBBLGYod1YJckx9a2Ar+uJWcKGqM/0=";
   }

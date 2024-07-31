fcafc7f3fed7a6f2976286dce12854be9a6474bc
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-31 18:04:10 +0200

    chore(qemu): test

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -19,38 +19,28 @@
     VIRSH_GPU_AUDIO=pci_0000_01_00_1
   '';
   qemu = pkgs.writeShellScriptBin "qemu" ''
-    #
-    # Author: Sebastiaan Meijer (sebastiaan@passthroughpo.st)
-    #
-    # Copy this file to /etc/libvirt/hooks, make sure it's called "qemu".
-    # After this file is installed, restart libvirt.
-    # From now on, you can easily add per-guest qemu hooks.
-    # Add your hooks in /etc/libvirt/hooks/qemu.d/vm_name/hook_name/state_name.
-    # For a list of available hooks, please refer to https://www.libvirt.org/hooks.html
-    #
-
+    set -e
     GUEST_NAME="$1"
     HOOK_NAME="$2"
     STATE_NAME="$3"
     MISC="''${@:4}"
-
     BASEDIR="$(dirname $0)"
-
     HOOKPATH="$BASEDIR/qemu.d/$GUEST_NAME/$HOOK_NAME/$STATE_NAME"
-
-    set -e # If a script exits with an error, we should as well.
-
-    # check if it's a non-empty executable file
     if [ -f "$HOOKPATH" ] && [ -s "$HOOKPATH"] && [ -x "$HOOKPATH" ]; then
         eval \"$HOOKPATH\" "$@"
     elif [ -d "$HOOKPATH" ]; then
         while read file; do
-            # check for null string
             if [ ! -z "$file" ]; then
               eval \"$file\" "$@"
             fi
         done <<< "$(find -L "$HOOKPATH" -maxdepth 1 -type f -executable -print;)"
     fi
+  '';
+  start = pkgs.writeShellScriptBin "start.sh" ''
+    echo "executing start.sh"
+  '';
+  stop = pkgs.writeShellScriptBin "stop.sh" ''
+    echo "executing stop.sh"
   '';
 in
   with lib; {
@@ -83,6 +73,13 @@ in
 
               ln -sf ${kvm-conf} /var/lib/libvirt/hooks/kvm.conf
               ln -sf ${qemu}/bin/qemu /var/lib/libvirt/hooks/qemu
+              ln -sf ${start}/bin/start.sh /var/lib/libvirt/hooks/qemu.d/win11/prepare/begin/start.sh
+              ln -sf ${stop}/bin/stop.sh /var/lib/libvirt/hooks/qemu.d/win11/release/end/stop.sh
+
+              chmod +x /var/lib/libvirt/hooks/qemu
+              chmod +x /var/lib/libvirt/hooks/kvm.conf
+              chmod +x /var/lib/libvirt/hooks/qemu.d/win11/prepare/begin/start.sh
+              chmod +x /var/lib/libvirt/hooks/qemu.d/win11/release/end/stop.sh
             '';
           };
         };

11320784ead0f054d034ff4d5286b6f7a155b9ac
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-31 20:43:01 +0200

    chore(qemut): debug

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -44,18 +44,27 @@
     source ${kvm-conf}/bin/kvm.conf
     echo "Stopping display manager" >> $debugfile
     systemctl stop display-manager.service
+    systemctl isolate multi-user.target
+    while systemctl is-active --quiet "display-manager.service"; do
+      sleep 1
+    done
     echo "Tuning CPU settings" >> $debugfile
     systemctl set-property --runtime -- user.slice AllowedCPUs=0
     systemctl set-property --runtime -- system.slice AllowedCPUs=0
     systemctl set-property --runtime -- init.scope AllowedCPUs=0
     echo "Unloading amdgpu driver" >> $debugfile
+    modprobe -r drm_kms_helper
     modprobe -r amdgpu
+    modprobe -r radeon
+    modprobe -r drm
     sleep 3
     echo "Detaching PCI devices" >> $debugfile
-    virsh nodedev-detach $VIRSH_GPU_VIDEO
-    virsh nodedev-detach $VIRSH_GPU_AUDIO
+    # virsh nodedev-detach $VIRSH_GPU_VIDEO
+    # virsh nodedev-detach $VIRSH_GPU_AUDIO
     echo "Loading VFIO PCI driver" >> $debugfile
-    modprobe vfio-pci
+    modprobe vfio
+    modprobe vfio_pci
+    modprobe vfio_iommu_type1
   '';
   stop = pkgs.writeShellScriptBin "stop.sh" ''
     exec 19>/home/${user}/startlogfile

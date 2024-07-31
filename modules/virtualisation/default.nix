2a309cb2ab9e0229e1bbb4fce15a29fee4cfa994
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-31 17:58:26 +0200

    chore(qemu): test

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -14,6 +14,44 @@
     })
     .fd;
   pcids = ["1002:744c" "1002:ab30"];
+  kvm-conf = pkgs.writeText "kvm.conf" ''
+    VIRSH_GPU_VIDEO=pci_0000_01_00_0
+    VIRSH_GPU_AUDIO=pci_0000_01_00_1
+  '';
+  qemu = pkgs.writeShellScriptBin "qemu" ''
+    #
+    # Author: Sebastiaan Meijer (sebastiaan@passthroughpo.st)
+    #
+    # Copy this file to /etc/libvirt/hooks, make sure it's called "qemu".
+    # After this file is installed, restart libvirt.
+    # From now on, you can easily add per-guest qemu hooks.
+    # Add your hooks in /etc/libvirt/hooks/qemu.d/vm_name/hook_name/state_name.
+    # For a list of available hooks, please refer to https://www.libvirt.org/hooks.html
+    #
+
+    GUEST_NAME="$1"
+    HOOK_NAME="$2"
+    STATE_NAME="$3"
+    MISC="''${@:4}"
+
+    BASEDIR="$(dirname $0)"
+
+    HOOKPATH="$BASEDIR/qemu.d/$GUEST_NAME/$HOOK_NAME/$STATE_NAME"
+
+    set -e # If a script exits with an error, we should as well.
+
+    # check if it's a non-empty executable file
+    if [ -f "$HOOKPATH" ] && [ -s "$HOOKPATH"] && [ -x "$HOOKPATH" ]; then
+        eval \"$HOOKPATH\" "$@"
+    elif [ -d "$HOOKPATH" ]; then
+        while read file; do
+            # check for null string
+            if [ ! -z "$file" ]; then
+              eval \"$file\" "$@"
+            fi
+        done <<< "$(find -L "$HOOKPATH" -maxdepth 1 -type f -executable -print;)"
+    fi
+  '';
 in
   with lib; {
     imports = [
@@ -42,6 +80,9 @@ in
               mkdir -p /var/lib/libvirt/hooks/qemu.d/win11/prepare/begin
               mkdir -p /var/lib/libvirt/hooks/qemu.d/win11/release/end
               mkdir -p /var/lib/libvirt/vgabios
+
+              ln -sf ${kvm-conf} /var/lib/libvirt/hooks/kvm.conf
+              ln -sf ${qemu}/bin/qemu /var/lib/libvirt/hooks/qemu
             '';
           };
         };
@@ -78,17 +119,7 @@ in
           "iommu=pt"
           "hugepagesz=1G"
           "hugepages=24"
-          # ("vfio-pci.ids=" + builtins.concatStringsSep "," pcids)
         ];
-        # blacklistedKernelModules = ["amdgpu"];
-        # postBootCommands = ''
-        #   DEVS="0000:0f:00.0 0000:0f:00.1"
-        #
-        #   for DEV in $DEVS; do
-        #     echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
-        #   done
-        #   modprobe -i vfio-pci
-        # '';
       };
       virtualisation = {
         libvirtd = {

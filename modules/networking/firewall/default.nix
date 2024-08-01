afa567cc634dcf55af40c3deedba00a11115d967
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-01 09:23:42 +0200

    chore(firewall): update settings

--- modules/networking/firewall/default.nix
+++ modules/networking/firewall/default.nix
@@ -16,25 +16,22 @@ in
       };
     };
     config = mkIf (cfg.enable && cfg.firewall.enable) {
-      boot = {
-        kernel = {
-          sysctl = {
-            "net.ipv4.ip_forward" = "1";
+      networking = {
+        firewall = {
+          enable = cfg.firewall.enable;
+        };
+        bridges = {
+          br0 = {
+            interfaces = ["wlp4s0"];
           };
         };
-      };
-      networking = {
-        firewall = let
-          vnc = 5900;
-        in {
-          enable = cfg.firewall.enable;
-          allowedTCPPorts = [vnc];
-          extraCommands = ''
-            iptables -A FORWARD -o virbr0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-            iptables -A FORWARD -i virbr0 -o wlp4s0 -j ACCEPT
-            iptables -t nat -A POSTROUTING -s 192.168.122.0/24 -o eth0 -j MASQUERADE
-            iptables -t nat -A PREROUTING -i wlp4s0 -p tcp --dport 5900 -j DNAT --to-destination 192.168.122.100:5900
-          '';
+        interfaces = {
+          wlp4s0 = {
+            useDHCP = true;
+          };
+          br0 = {
+            useDHCP = true;
+          };
         };
       };
     };

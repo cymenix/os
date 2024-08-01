4a13e12a73e0e5f58caad71cd5cbea297d12e5b9
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-01 08:53:18 +0200

    chore(firewall): test vnc connection

--- modules/networking/firewall/default.nix
+++ modules/networking/firewall/default.nix
@@ -16,12 +16,25 @@ in
       };
     };
     config = mkIf (cfg.enable && cfg.firewall.enable) {
+      boot = {
+        kernel = {
+          sysctl = {
+            "net.ipv4.ip_forward" = "1";
+          };
+        };
+      };
       networking = {
-        firewall = {
+        firewall = let
+          vnc = 5900;
+        in {
           enable = cfg.firewall.enable;
-          allowedTCPPorts = [
-            5900 # VNC
-          ];
+          allowedTCPPorts = [vnc];
+          extraCommands = ''
+            iptables -A FORWARD -o virbr0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+            iptables -A FORWARD -i virbr0 -o wlp4s0 -j ACCEPT
+            iptables -t nat -A POSTROUTING -s 192.168.122.0/24 -o eth0 -j MASQUERADE
+            iptables -t nat -A PREROUTING -i wlp4s0 -p tcp --dport 5900 -j DNAT --to-destination 192.168.122.100:5900
+          '';
         };
       };
     };

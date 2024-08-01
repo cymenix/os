5cbabb0df7d17e25f9823f44a7f0acc4bdd10adf
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-01 12:23:03 +0200

    chore(firewall): test

--- modules/networking/firewall/default.nix
+++ modules/networking/firewall/default.nix
@@ -18,6 +18,10 @@ in
       };
     };
     config = mkIf (cfg.enable && cfg.firewall.enable) {
+      boot.kernel.sysctl = {
+        "net.ipv4.conf.all.forwarding" = 1;
+        "net.ipv4.conf.default.forwarding" = 1;
+      };
       networking = {
         dhcpcd = {
           denyInterfaces = ["virbr0"];
@@ -26,9 +30,7 @@ in
           enable = cfg.firewall.enable;
           allowedTCPPorts = [vnc];
           extraCommands = ''
-            iptables -A FORWARD -s 192.168.2.0/24 -d 192.168.122.0/24 -o virbr0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-            iptables -D FORWARD -o virbr0 -p tcp -d ${vmip} --dport ${builtins.toString vnc} -j ACCEPT
-            iptables -t nat -D PREROUTING -p tcp --dport ${builtins.toString vnc} -j DNAT --to ${vmip}:${builtins.toString vnc}
+            iptables -t nat -A POSTROUTING -d ${vmip} -p tcp -m tcp --dport ${builtins.toString vnc} -j MASQUERADE"
           '';
         };
         nat = {
